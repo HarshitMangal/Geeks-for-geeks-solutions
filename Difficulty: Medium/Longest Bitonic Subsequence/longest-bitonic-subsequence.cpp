0c57d6d4cc83b7197a1774c85807b26ad9840b9d
Harshit Mangal <mangalharsh54@gmail.com>
2026-07-15 00:05:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Bitonic Subsequence/longest-bitonic-subsequence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    int longestBitonicSequence(int n, vector<int> &nums) {
+        vector<int>lis(n,1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+               if(nums[i]>nums[j]){
+                   lis[i]=max(lis[i],lis[j]+1);
+               }
+            }
+        }
+        vector<int>lds(n,1);
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>i;j--){
+                if(nums[i]>nums[j]){
+                    lds[i]=max(lds[i],lds[j]+1);
+                }
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(lis[i]>1&&lds[i]>1){
+                ans=max(ans,lis[i]+lds[i]-1);
+            }
+        }
+        return ans;
+        
+    }
+};

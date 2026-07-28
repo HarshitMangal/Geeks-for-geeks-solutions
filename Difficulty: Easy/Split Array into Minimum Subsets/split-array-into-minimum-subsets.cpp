b82921e69790a1dcfc2c472246e762a0537599b3
Harshit Mangal <mangalharsh54@gmail.com>
2026-07-29 01:03:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Split Array into Minimum Subsets/split-array-into-minimum-subsets.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int minSubsets(vector<int>& arr) {
+        // Your code here
+        sort(arr.begin(),arr.end());
+        int ans = 1;
+        for(int i = 1;i<arr.size();i++){
+            if(arr[i]!=arr[i-1]+1) ans++;
+        }
+        return ans;
+    }
+};
+
